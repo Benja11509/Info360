61891ffc5438d9eaaf11a8d694535695b7f04d13
ILÁN URIEL BUSSO <48857364@ort>
2025-10-30 14:36:43 -0300

    14:30

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -9,36 +9,36 @@ public class Usuario
     [JsonProperty]
     public string nombreUsuario { get; private set; }
     [JsonProperty]
-    public string nombre? { get; private set; }
+    public string? nombre { get; private set; }
     [JsonProperty]
     public string contraseña { get; private set; }
     [JsonProperty]
-    public string apellido? { get; private set; }
+    public string? apellido { get; private set; }
     [JsonProperty]
     public DateTime? fechaNacimiento { get; private set; }
     [JsonProperty]
     public string tipoUsuario { get; private set; }
     [JsonProperty]
-    public string telefono { get; private set; }
+    public string? telefono { get; private set; }
     [JsonProperty]
-    public int nivelApoyo { get; private set; }
+    public int? nivelApoyo { get; private set; }
     [JsonProperty]
     public DateTime? fechaIngreso { get; private set; }
     [JsonProperty]
-    public int puntos { get; private set; }
+    public int? puntos { get; private set; }
     [JsonProperty]
     public string mail { get; private set; }
     [JsonProperty]
-    public string fotoPerfil { get; private set; }
+    public string? fotoPerfil { get; private set; }
 
 
 
-    public Usuario(string nombreUsuario, string contraseña, string mail, string tipoUser)
+    public Usuario(string nombreUsuario, string contraseña, string mail, string tipoUsuario)
     {
         this.nombreUsuario = nombreUsuario;
         this.contraseña = contraseña;
         this.mail = mail;
-        this.tipoUsuario = tipoUser;
+        this.tipoUsuario = tipoUsuario;
 
     }
     public Usuario()
