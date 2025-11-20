7db70255ccdbc5273b8be68c8373c55e799ad96c
MATEO JULIÁN MILANO <49432878@ort>
2025-11-20 09:04:45 -0300

    primero jueves

--- obj/Debug/net8.0/Info360.AssemblyInfo.cs
+++ obj/Debug/net8.0/Info360.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Info360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba1b22891408da69bb3134251617e496fac4820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8ae941209eb7827f75f4dc579bd932d50f0ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Info360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Info360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
